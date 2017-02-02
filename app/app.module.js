19810cfe4840ff7157ddf37641a0cf050e4db748
Cyril TOVENA <cyril.tovena@ubisoft.com>
2017-02-02 16:41:28 -0500

    adding category

--- app/app.module.js
+++ app/app.module.js
@@ -1,12 +1,16 @@
 angular
 	.module('UbiHeard',['ngRoute']);
-
+var categories = ['Gameplay', 'Story' , 'Art' , 'User Interface' , 'Other'];
+if(!window.localStorage.getItem('categories')) {
+	window.localStorage.setItem('categories',JSON.stringify(categories));
+}
 	var suggestions = [{
 		"id": 1,
 		"from": {
 			"picture": "fake url",
 			"name": "Jhon do"
 		},
+		"category" : 'Gameplay',
 		"title": "suggestion title",
 		"description": "sugesstion text",
 		"comments": [{
@@ -21,6 +25,7 @@ angular
 		"votes": 500
 	}, {
 		"id": 2,
+		"category" : 'Gameplay',
 		"from": {
 			"picture": "fake url",
 			"name": "Jhon do"
@@ -39,6 +44,7 @@ angular
 		"votes": 500
 	}, {
 		"id": 3,
+		"category" : 'Gameplay',
 		"from": {
 			"picture": "fake url",
 			"name": "Jhon do"
