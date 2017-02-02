17e68edbf6cded86fadfc21831e30032907a3920
Cyril TOVENA <cyril.tovena@ubisoft.com>
2017-02-02 15:43:28 -0500

    adding json test

--- app/app.module.js
+++ app/app.module.js
@@ -1,2 +1,60 @@
 angular
 	.module('UbiHeard',['ngRoute']);
+
+	var suggestions = [{
+		"id": 1,
+		"from": {
+			"picture": "fake url",
+			"name": "Jhon do"
+		},
+		"title": "suggestion title",
+		"description": "sugesstion text",
+		"comments": [{
+			"from": {
+				"picture": "fake url",
+				"name": "Jhon do"
+			},
+			"createdAt": "date",
+			"message": "comment message"
+		}],
+		"createdAt": "date",
+		"votes": 500
+	}, {
+		"id": 2,
+		"from": {
+			"picture": "fake url",
+			"name": "Jhon do"
+		},
+		"title": "suggestion title",
+		"description": "sugesstion text",
+		"comments": [{
+			"from": {
+				"picture": "fake url",
+				"name": "Jhon do"
+			},
+			"createdAt": "date",
+			"message": "comment message"
+		}],
+		"createdAt": "date",
+		"votes": 500
+	}, {
+		"id": 3,
+		"from": {
+			"picture": "fake url",
+			"name": "Jhon do"
+		},
+		"title": "suggestion title",
+		"description": "sugesstion text",
+		"comments": [{
+			"from": {
+				"picture": "fake url",
+				"name": "Jhon do"
+			},
+			"createdAt": "date",
+			"message": "comment message"
+		}],
+		"createdAt": "date",
+		"votes": 500
+	}
+];
+Window.localStorage.setItem('suggestions',suggestions);
