a603f9d545b96c9d513231bb76958adb4fbefb57
erick <erick@hotmail.ca>
2017-02-03 11:10:02 -0500

    Filters and ordering for listing

--- app/listings.controller.js
+++ app/listings.controller.js
@@ -6,8 +6,13 @@ function ListingController() {
     var vm = this;
 
     vm.suggestions = JSON.parse(window.localStorage.getItem('suggestions'));
+    vm.orderingProperty = '-votes';
+    vm.categories =  JSON.parse(window.localStorage.getItem('categories'));
+    vm.categories.unshift('All');
+    vm.selectedCategory = vm.categories[0];
 
     vm.addVote = addVote;
+    vm.sortBy = sortBy;
 
     function addVote(id) {
         var sg = _.find(vm.suggestions, ['id', id]);
@@ -19,6 +24,11 @@ function ListingController() {
           sg.voted = false;
         }
         window.localStorage.setItem('suggestions',JSON.stringify(vm.suggestions));
+    }
+
+    function sortBy(property) {
+        vm.orderingProperty = property;
+        console.log(vm.orderingProperty);
     }
 
 
