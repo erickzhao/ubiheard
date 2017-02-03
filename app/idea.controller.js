9ad1851eea44d603f8587127020a5b82d4cc7b0e
sepHed <sephedoo@gmail.com>
2017-02-03 10:59:33 -0500

    Adding comments

--- app/idea.controller.js
+++ app/idea.controller.js
@@ -1,15 +1,32 @@
 angular
     .module('UbiHeard')
-    .controller('IdeaController',['$routeParams',IdeaController]);
+    .controller('IdeaController', ['$routeParams', IdeaController]);
 
-function IdeaController($routeParams){
-   
+function IdeaController($routeParams) {
+
     var vm = this;
     vm.suggestions = JSON.parse(window.localStorage.getItem('suggestions'));
     vm.id = parseInt($routeParams.ID);
-    vm.selected = _.find(vm.suggestions,['id',vm.id]);
-    vm.comments = vm.selected.comments;
+    vm.selected = _.find(vm.suggestions, ['id', vm.id]);
+    vm.content = "";
+    vm.addComment = addComment;
 
-   // console.log(selected);
+
+    function addComment() {
+        vm.selected.comments.push({
+            "from": {
+                "picture": getRandomFace(),
+                "name": "Me"
+            },
+            "createdAt": new Date(Date.now() + -5 * 24 * 3600 * 1000),
+            "message": vm.content
+        });
+        window.localStorage.setItem('suggestions', JSON.stringify(vm.suggestions));
+        vm.content= "";
+    }
+
+    function getRandomFace() {
+	return "https://randomuser.me/api/portraits/men/"+ Math.floor(Math.random() * 99)+".jpg";
+}
 
 }
